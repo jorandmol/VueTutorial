e797799e0b58e61ca0cd9f67511422ab1ed6a367
jorandmol <jandrea@us.es>
2022-06-18 17:44:56 +0200

    Using for loops

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-<<<<<<< HEAD
-            stock: 11,
-            onSale: true
-=======
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
->>>>>>> origin/L5-start
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+            ],
+            sizes: ['XS', 'S', 'M', 'L', 'XL']
         }
     }
 })
